521b13bc318a302f84c4280e4bece9dac04c6f14
petros0979 <petrosmemiru@gmail.com>
2024-11-07 11:35:01 +0000

    best

---
+++ 0x03-React_component/task_4/dashboard/src/Footer/Footer.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './Footer.css';
+import { getFullYear, getFooterCopy } from '../utils/utils';
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <p>
+        Copyright {getFullYear()} - {getFooterCopy(true)}
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
+
