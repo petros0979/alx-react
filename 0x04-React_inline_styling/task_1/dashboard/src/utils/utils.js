7f37468a4d878b580e90dbec0cc14430cc8c8947
petros0979 <petrosmemiru@gmail.com>
2024-11-12 15:56:39 +0000

    best

---
+++ 0x04-React_inline_styling/task_1/dashboard/src/utils/utils.js
@@ -0,0 +1,4 @@
+
+export const getFooterCopy = (isIndex) => (isIndex ? "Holberton School" : "Holberton School main dashboard");
+
+export const getLatestNotification = () => "<strong>Urgent Requirement</strong> - complete by EOD";
