ecefa6deb2671662ffb87e9c48cba07c243a0ba5
root <petrosmemiru@gmail.com>
2024-11-21 16:27:13 +0000

    best

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -4,10 +4,8 @@ export function concatElements(page1, page2) {
   const list1 = List(page1);
   const list2 = List(page2);
 
-
   return list1.concat(list2);
 }
-
 
 export function mergeElements(page1, page2) {
   const map1 = Map(page1);
